c9db620227df9af1c17d3d05ef5c1e782ac620b5
Ian Bigford <ibigford9@gmail.com>
2026-02-18 23:14:37 -0500

    quick fix

--- components/AgentforceChat.tsx
+++ components/AgentforceChat.tsx
@@ -5,15 +5,15 @@ import Script from "next/script";
 export default function AgentforceChat() {
   return (
     <Script
-      src="https://orgfarm-234f3aac10--devpro.sandbox.my.site.com/ESWNextjsChannel1771471986612/assets/js/bootstrap.min.js"
+      src="https://orgfarm-234f3aac10--devpro.sandbox.my.site.com/ESWNextjsChannel21771474146729/assets/js/bootstrap.min.js"
       strategy="afterInteractive"
       onLoad={() => {
         try {
           (window as any).embeddedservice_bootstrap.settings.language = 'en_US';
           (window as any).embeddedservice_bootstrap.init(
             '00DAs00000tKZZs',
-            'Nextjs_Channel',
-            'https://orgfarm-234f3aac10--devpro.sandbox.my.site.com/ESWNextjsChannel1771471986612',
+            'Nextjs_Channel2',
+            'https://orgfarm-234f3aac10--devpro.sandbox.my.site.com/ESWNextjsChannel21771474146729',
             {
               scrt2URL: 'https://orgfarm-234f3aac10--devpro.sandbox.my.salesforce-scrt.com'
             }
